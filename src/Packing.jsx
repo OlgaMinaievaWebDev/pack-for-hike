3af4f606edd01fad236b51073f4594f3b3099244
Olga <minaeva9@gmail.com>
2024-05-06 14:42:08 -0400

    start component css

--- src/Packing.jsx
+++ src/Packing.jsx
@@ -1,3 +1,3 @@
 export default function Packing() {
-  return <>Start packing</>;
+  return <>Enter packing form</>;
 }
--- src/Start.jsx
+++ src/Start.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
+
 export default function Start({ setStartPacking }) {
+  const [weight, setWeight] = useState(20);
+
   function handleStartApp() {
     setStartPacking(true);
   }
   return (
     <div className="start-page">
-      <h1>Welcome to Backpack Packing App</h1>
+      <h1>Lets pack for a hike</h1>
+      <p>Choose your backpack capacity</p>
+      <select
+        className="choose-weight"
+        value={weight}
+        onChange={(e) => setWeight(e.target.value)}
+      >
+        {Array.from({ length: 55 }, (_, index) => index + 20).map(
+          (num, index) =>
+            index % 5 === 0 && <option key={num}>{num} lbs</option>
+        )}
+      </select>
       <button className="btn-start" onClick={handleStartApp}>
         Start Packing
       </button>
