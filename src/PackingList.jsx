77de65f868e764fac727ac1b79bf9371948f6883
Olga <minaeva9@gmail.com>
2024-05-14 12:12:25 -0400

    action menu added

--- src/PackingList.jsx
+++ src/PackingList.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import PackingItem from "./PackingItem";
 
 export default function PackingList({
@@ -6,10 +7,20 @@ export default function PackingList({
   onToggleItem,
   onClearList,
 }) {
+  const [sortedBy, setSortedBy] = useState("input");
+  let sortedItems = [];
+  if (sortedBy === "input") {
+    sortedItems = items;
+  } else if (sortedBy === "alphabetically") {
+    sortedItems = items.slice().sort((a, b) => a.name.localeCompare(b.name));
+  } else if (sortedBy === "packed") {
+    sortedItems = items.slice().sort((a, b) => a.packed - b.packed);
+  }
+
   return (
     <div className="items-list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <PackingItem
             item={item}
             key={item.id}
@@ -19,7 +30,19 @@ export default function PackingList({
           />
         ))}
       </ul>
-      {items.length > 0 && <button onClick={onClearList}>Clear List</button>}
+      {items.length > 0 && (
+        <div className="actions">
+          <select
+            value={sortedBy}
+            onChange={(e) => setSortedBy(e.target.value)}
+          >
+            <option value="input">Sort by input order</option>
+            <option value="alphabetically">Sort alphabetically</option>
+            <option value="packed">Sort by packed status</option>
+          </select>
+          <button onClick={onClearList}>Clear List</button>
+        </div>
+      )}
     </div>
   );
 }
